017216cd447f1859e3eea731d673158f21029910
MdBillG <mdb8854@gmail.com>
2024-04-30 20:58:32 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -3,24 +3,145 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-        <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+
+    <div className='bg-gray-600 mt-20'>
+    <div className="bg-gray-150 flex gap-6 ml-36">
+   <div className='   w-80  text-center bg-gray-600  text-gray-700 dark:text-gray-300 transition-colors duration-200 transform rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700'> Intro
+   <div >
+    <h1 className=' text-4xl font-semibold  text-gray-700 dark:text-gray-300'>$19 <span className='text-base font-normal'>/ Month</span></h1> 
+    <h1>For most businesses that want to optimaize web queries.</h1>
+      <div className='flex flex-col ml-16 mt-6'>
+        <div className='flex '>
+       <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+      <span class="mx-4 text-gray-700 dark:text-gray-300">All limited links</span>
+        </div>
+        <div className='flex mt-2 '>
+       <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+      <span class="mx-4 text-gray-700 dark:text-gray-300">Own analytics platform</span>
+        </div>
+        <div className='flex mt-2 '>
+       <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+      <span class="mx-4 text-gray-700 dark:text-gray-300">Chat support</span>
+        </div>
+        <div className='flex  mt-2'>
+       <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+      <span class="mx-4 text-gray-700 dark:text-gray-300">Unlimited users</span>
+        </div>
+        <div className='flex  mt-2'>
+       <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+      <span class="mx-4 text-gray-700 dark:text-gray-300">Chat support</span>
+        </div>
+    </div>
+   
+     <div className=' border-2 mt-6 w-full px-4 py-2 mt-10 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600'>
+                     
+      <button className=' '>
+      Choose a Plan</button>
+      </div>
+     
+   </div>
+   </div>
+   <div  className='  w-80 text-center bg-gray-600  text-gray-700 dark:text-gray-300 transition-colors duration-200 transform rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700'>Base
+   <div>
+    <h1 className='text-4xl font-semibold  text-gray-700 dark:text-gray-300'>$29 <span className=' text-base font-normal'>/ Month</span></h1>
+    <h1>For most businesses that want to optimaize web queries.</h1>
+    <div className='flex flex-col ml-16 mt-6'>
+        <div className='flex '>
+       <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+      <span class="mx-4 text-gray-700 dark:text-gray-300">All limited links</span>
+        </div>
+        <div className='flex  mt-2'>
+       <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+      <span class="mx-4 text-gray-700 dark:text-gray-300">Own analytics platform</span>
+        </div>
+        <div className='flex  mt-2'>
+       <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+      <span class="mx-4 text-gray-700 dark:text-gray-300">Chat support</span>
+        </div>
+        <div className='flex  mt-2'>
+       <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+      <span class="mx-4 text-gray-700 dark:text-gray-300">Unlimited users</span>
+        </div>
+        <div className='flex  mt-2'>
+       <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+      <span class="mx-4 text-gray-700 dark:text-gray-300">Chat support</span>
+        </div>
+    </div>
+
+   
+      <div className='border-2  mt-6  border-2 mt-6 w-full px-4 py-2 mt-10 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600'>
+      <button className=' '>
+      Choose a Plan</button>
+      </div>
+    
+    </div>
+    </div>
+   <div  className='  w-80 text-center bg-gray-600 text-gray-700 dark:text-gray-300 transition-colors duration-200 transform rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700'> Popular
+   <div>
+    </div>
+    <h1 className='text-4xl font-semibold  text-gray-700 dark:text-gray-300' >$35 <span className='text-base font-normal'>/Month</span></h1>
+    <h1>For most businesses that want to optimaize web queries.</h1>
+   <div className='flex flex-col ml-16 mt-6'>
+        <div className='flex '>
+       <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+      <span class="mx-4 text-gray-700 dark:text-gray-300">All limited links</span>
+        </div>
+        <div className='flex  mt-2'>
+       <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+      <span class="mx-4 text-gray-700 dark:text-gray-300">Own analytics platform</span>
+        </div>
+        <div className='flex  mt-2'>
+       <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+      <span class="mx-4 text-gray-700 dark:text-gray-300">Chat support</span>
+        </div>
+        <div className='flex  mt-2'>
+       <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+      <span class="mx-4 text-gray-700 dark:text-gray-300">Unlimited users</span>
+        </div>
+        <div className='flex  mt-2'>
+       <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+      <span class="mx-4 text-gray-700 dark:text-gray-300">Chat support</span>
+        </div>
+    </div>
+    <div className='border-2  mt-6  border-2 mt-6 w-full px-4 py-2 mt-10 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600'>
+      <button className=' '>
+      Choose a Plan</button>
+      </div>
+ 
+    </div>
+    </div>
+    
+
     </div>
   );
 }
